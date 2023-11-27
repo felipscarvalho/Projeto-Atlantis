7bf1c86e5320344f1542edb54b11ac67bdfe4292
PsyShadowF <felipecarvalho5520@gmail.com>
2023-11-27 12:09:26 -0300

    First version of models made.

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Species
 
 # Register your models here.
+
+
+class SpeciesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'scientific_name', 'status']
+
+
+admin.site.register(Species, SpeciesAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class StatusOptions(models.IntegerChoices):
+    extinct = 0, 'Extinto'
+    threatened_with_extinction = 1, 'Ameaçado de Extinção'
+
+
+class Species(models.Model):
+    image = models.ImageField(upload_to='animals/',
+                              default='defaults/animal_default.png')
+    name = models.CharField(max_length=255, verbose_name='Nome')
+    scientific_name = models.CharField(
+        max_length=255, verbose_name='Nome Científico')
+    status = models.IntegerField(
+        choices=StatusOptions.choices, verbose_name='Status')
+    description = models.TextField(verbose_name='Descrição')
