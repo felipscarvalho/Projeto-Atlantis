2bf5c33c7758688325610d35aeed5210285d9a20
PsyShadowF <felipecarvalho5520@gmail.com>
2023-12-04 12:36:13 -0300

    Hero of the landing page done.

--- atlantis/settings.py
+++ atlantis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*qte5fyll&=!s7-y-c9vd&q3!m#y@jw!pxm#0yn^nbgaq32eea
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
